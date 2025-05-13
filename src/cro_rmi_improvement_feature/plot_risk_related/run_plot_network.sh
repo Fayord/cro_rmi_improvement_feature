77f066439501958a980fd776a45264e33cd0e5a8
Fayord <fordthanatorn@gmail.com>
2025-05-13 14:34:17 +0700

    .

--- src/cro_rmi_improvement_feature/plot_risk_related/run_plot_network.sh
+++ src/cro_rmi_improvement_feature/plot_risk_related/run_plot_network.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /Users/ford/Documents/coding_trae/cro_rmi_improvement_feature/src/cro_rmi_improvement_feature/plot_risk_related/plot_network.py --port 7070
+python3 ./plot_network.py --port 7070
